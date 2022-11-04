5acea982166ac37876ea31ff7ff43faed1472e82
josemeza <nachomezalk24@gmail.com>
2022-11-03 23:22:09 -0300

    Problemas para renderizar ItemDetailContainer

--- src/components/ItemListContainer/ItemLinstContainer.jsx
+++ src/components/ItemListContainer/ItemLinstContainer.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Item from "../Item/Item";
+import getItemsFromAPI from "../../mockService/mockService";
+
 
 function ItemlistContainer (props){
-    return (
-    <>
-    <h3 className="m-3">{props.children}</h3>
-    </>
-    );
+    const [productsList, setProductsList] = useState([]);
+  useEffect(() => {
+
+        getItemsFromAPI().then((itemsDB) => {
+            setProductsList(itemsDB);
+        });
+    }, []);
+
+  return <Item productsList={productsList} />;
 }
 
 export default ItemlistContainer;
